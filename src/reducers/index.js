edffeb53ab1152d355c81e546178ea8a43783ede
Astri Jeff Chung <jeffchung@astri.org>
2017-12-19 11:42:54 +0800

    reuse network component

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,13 @@
 import count from './count.js';
 import network from './network.js';
 import domain from './domain';
-
+import Api from 'redux-api-middleware-addon';
 import nav from './nav.js';
 const rootReducer = function(state = {}, action) {
   return {
     count: count(state.count, action),
     domain: domain(state.domain, action),
-    network: network(state.network, action),
+    network: Api.Reducer.network(state.network, action),
     nav: nav(state.nav, action),
   };
 };
